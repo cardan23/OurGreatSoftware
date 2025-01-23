7f7e5374a5d4ccd59c4b750db1886dd642ef331e
hph111 <hph111@gmail.com>
2025-01-22 23:26:55 -0600

    Primera parte del login

---
+++ src/Components/LoginForm/LoginForm.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./LoginForm.css"
+
+
+export const LoginForm = () => {
+  return (
+    <div className='wrapper'>
+        <form  action="">
+            <h1>Login</h1>
+            <div className="input-box">
+                <input type="text" placeholder='User' />
+            </div>
+        </form>
+        
+
+    </div>
+  )
+}

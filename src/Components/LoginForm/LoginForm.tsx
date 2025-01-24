ee41f79e4cebe4a4e0982e01fdf02fdca9a2ec60
hph111 <hph111@gmail.com>
2025-01-23 22:12:17 -0600

    Mejorando diseño de Login

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import "./LoginForm.css"
 
+import "./LoginForm.css"
+import { FaUser, FaLock } from "react-icons/fa";
 
 export const LoginForm = () => {
   return (
@@ -8,8 +8,22 @@ export const LoginForm = () => {
         <form  action="">
             <h1>Login</h1>
             <div className="input-box">
-                <input type="text" placeholder='User' />
+                <input type="text" placeholder='Usename' required />
+                <FaUser className="icon" />
             </div>
+            <div className="input-box">
+                <input type="password" placeholder='Password' required />
+                <FaLock className="icon" />
+            </div>
+            <div className="remember-forgot">
+              <label><input type="checkbox" />Remember me</label>
+              <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit">Login</button>
+            <div className="register-link">
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>            
+
         </form>
         
 
